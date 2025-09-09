9a7c119838753f4328a6f6b9cb69771d0cf7dce0
bjohnson <blake.johnson@osii.com>
2025-09-09 10:39:07 -0500

    both electron and web working

--- dev/src/ts/profile/ProfileManager.ts
+++ dev/src/ts/profile/ProfileManager.ts
@@ -2,7 +2,7 @@ import { ServiceProvider } from "../util/ServiceProvider.js";
 import { BlobMetaData } from '../blob/BlobMetaData.js';
 import { BlobId, StorageKeys } from "../resources/constants.js";
 import { Profile } from "./Profile.js";
-import * as dProfile from "../../assets/config/defaultProfile.json" with {type: 'json'};
+import * as dProfile from "@assets/config/defaultProfile.json" with {type: 'json'};
 import { ILogObj, Logger } from "tslog";
 
 //const _logger = ServiceProvider.logService.createNewLogger("ProfileManager");
